72a000a63d8ffd8431ca2ca4509ba34214076f36
Bharpur Singh <bharpursingh3081@outlook.com>
2024-07-04 21:13:36 -0400

    Table Responsiveness and Styling Added using Tailwind

--- ApplyMate-Frontend/src/components/ListJobsComponent.jsx
+++ ApplyMate-Frontend/src/components/ListJobsComponent.jsx
@@ -1,7 +1,4 @@
-import React from 'react'
-import '../../src/index.css'
-
-function ListJobsComponent() {
+const ListJobsComponent = () => {
 
     const dumyData = [
         {
@@ -42,45 +39,150 @@ function ListJobsComponent() {
             "dateApplied": "2024-02-24",
             "keywords": ["Go", "Full Stack", "React", "Front End", "Java"],
             "response": "Pending"
+        },
+
+        {
+            "id": "5",
+            "jobPosition": "Java Developer",
+            "jobDescription": "Java Full Stack Developer",
+            "datePosted": "2024-02-22",
+            "dateApplied": "2024-02-24",
+            "keywords": ["Go", "Full Stack", "React", "Front End", "Java"],
+            "response": "Interview"
+        },
+        {
+            "id": "6",
+            "jobPosition": "Go Developer",
+            "jobDescription": "Java Full Stack Developer",
+            "datePosted": "2024-02-22",
+            "dateApplied": "2024-02-24",
+            "keywords": ["Go", "Full Stack", "React", "Front End", "Java"],
+            "response": "Accepted"
+        },
+        {
+            "id": "7",
+            "jobPosition": "VBA Developer",
+            "jobDescription": "Java Full Stack Developer",
+            "datePosted": "2024-02-22",
+            "dateApplied": "2024-02-24",
+            "keywords": ["Go", "Full Stack", "React", "Front End", "Java"],
+            "response": "Rejected"
         }
     ]
 
     return (
-        <div>
-            <h2 className='text-center'>List of Jobs</h2>
-            <table className='table table-striped table-bordered'>
-                <thead>
-                    <tr>
+        <div className="p-5 h-screen bg-gray-100">
+            <h1 className="text-2xl mb-2 text-center">Job List</h1>
 
-                    </tr>
-                    <th>Job id</th>
-                    <th>Job Position</th>
-                    <th>Job Description</th>
-                    <th>Date Posted</th>
-                    <th>Date Applied</th>
-                    <th>Keywords</th>
-                    <th>Response</th>
+            <div className="p-5 h-screen overflow-auto rounded-lg shadow hidden md:block">
+                <table className="w-full">
+                    <thead className="bg-gray-50 border-b-2 border-gray-200 whitespace-nowrap">
+                        <tr>
+                            <th className="w-10 p-3 text-sm font-semibold tracking-wide text-lef">Job id</th>
+                            <th className="w-25 p-3 text-sm font-semibold tracking-wide text-left">Job Position</th>
+                            <th className="w-30 p-3 text-sm font-semibold tracking-wide text-left">Job Description</th>
+                            <th className="w-15 p-3 text-sm font-semibold tracking-wide text-left">Date Posted</th>
+                            <th className="w-15 p-3 text-sm font-semibold tracking-wide text-left">Date Applied</th>
+                            <th className="w-34 p-3 text-sm font-semibold tracking-wide text-left">Keywords</th>
+                            <th className="w-10 p-3 text-sm font-semibold tracking-wide text-left" >Response</th>
+                        </tr>
 
-                </thead>
-                <tbody>
-                    {
-                        dumyData.map(job =>
-                            <tr key={job.id}>
-                                <td>{job.id}</td>
-                                <td>{job.jobPosition}</td>
-                                <td>{job.jobDescription}</td>
-                                <td>{job.datePosted}</td>
-                                <td>{job.dateApplied}</td>
-                                <td>{job.keywords}</td>
-                                <td>{job.response}</td>
-                            </tr>
-                        )
-                    }
-                </tbody>
-            </table>
 
-        </div>
-    )
-}
+                    </thead>
+                    <tbody className="bg-white divide-y divide-gray-100 whitespace-nowrap">
+                        {
+                            dumyData.map(job =>
+                                <tr key={job.id} className={job.id % 2 === 0 ? 'bg-white' : 'bg-gray-200'}>
+                                    <td className='p-3 text-sm text-gray-700'>{job.id}</td>
+                                    <td className='p-3 text-sm text-gray-700'>{job.jobPosition}</td>
+                                    <td className='p-3 text-sm text-gray-700'>{job.jobDescription}</td>
+                                    <td className='p-3 text-sm text-gray-700'>{job.datePosted}</td>
+                                    <td className='p-3 text-sm text-gray-700'>{job.dateApplied}</td>
+                                    <td className='p-3 text-sm text-gray-700'>{job.keywords}</td>
+                                    <td className='p-3 text-sm text-gray-700'>
+                                        <span className={(() => {
+                                            switch (job.response.toLocaleLowerCase()) {
+                                                case 'pending':
+                                                    return 'p-1.5 text-xs font-medium uppercase tracking tracking-wider text-yellow-800 bg-yellow-200 rounded-lg bg-opacity-90'
+                                                case 'interview':
+                                                    return 'p-1.5 text-xs font-medium uppercase tracking tracking-wider text-yellow-800 bg-purple-200 rounded-lg bg-opacity-90'
+                                                case 'accepted':
+                                                    return 'p-1.5 text-xs font-medium uppercase tracking tracking-wider text-yellow-800 bg-green-200 rounded-lg bg-opacity-90'
+                                                case 'rejected':
+                                                    return 'p-1.5 text-xs font-medium uppercase tracking tracking-wider text-yellow-800 bg-red-200 rounded-lg bg-opacity-90'
+                                            }
+                                        })()
+                                        }
+                                        >{job.response}</span>
+                                    </td>
+                                </tr>
+                            )
+                        }
+                    </tbody>
+                </table>
+            </div >
+
+            <div className='grid grid-cols-1 gap-4 md:hidden'>
+                {dumyData.map(job => (
+                    <div key={job.id} className="bg-white p-4 space-y-4 rounded-lg shadow">
+                        <div className="flex items-center space-x-2 text-sm">
+                            <div>
+                                <div className="font-bold">Job Id:</div>
+                                <div>{job.id}</div>
+                            </div>
+                            <div>
+                                <div className="font-bold">Position:</div>
+                                <div>{job.jobPosition}</div>
+                            </div>
+                        </div>
+                        <div className="flex items-center space-x-2 text-sm">
+                            <div>
+                                <div className="font-bold">Description:</div>
+                                <div>{job.jobDescription}</div>
+                            </div>
+                        </div>
+                        <div className="flex items-center space-x-2 text-sm">
+                            <div>
+                                <div className="font-bold text-gray-500">Posted:</div>
+                                <div>{job.datePosted}</div>
+                            </div>
+                            <div>
+                                <div className="font-bold text-gray-500">Applied:</div>
+                                <div>{job.dateApplied}</div>
+                            </div>
+                        </div>
+                        <div className="flex items-center space-x-2 text-sm">
+                            <div>
+                                <div className="font-bold">Keywords:</div>
+                                <div>{job.keywords}</div>
+                            </div>
+                        </div>
+                        <div className="flex items-center space-x-2 text-sm">
+                            <div>
+                                <div className="font-bold">Response:</div>
+                                <span className={(() => {
+                                    switch (job.response.toLowerCase()) {
+                                        case 'pending':
+                                            return 'p-1.5 text-xs font-medium uppercase tracking tracking-wider text-yellow-800 bg-yellow-200 rounded-lg bg-opacity-90';
+                                        case 'interview':
+                                            return 'p-1.5 text-xs font-medium uppercase tracking tracking-wider text-yellow-800 bg-purple-200 rounded-lg bg-opacity-90';
+                                        case 'accepted':
+                                            return 'p-1.5 text-xs font-medium uppercase tracking tracking-wider text-yellow-800 bg-green-200 rounded-lg bg-opacity-90';
+                                        case 'rejected':
+                                            return 'p-1.5 text-xs font-medium uppercase tracking tracking-wider text-yellow-800 bg-red-200 rounded-lg bg-opacity-90';
+                                        default:
+                                            return '';
+                                    }
+                                })()}>{job.response}</span>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+        </div >
+
+    );
+};
 
 export default ListJobsComponent
